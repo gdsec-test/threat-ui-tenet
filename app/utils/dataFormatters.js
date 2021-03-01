b68beb4156d90a2c27729af5b0b1167c7935313d
ogomozov <ogomozov@godaddy.com>
2021-03-01 11:04:45 -0800

    Add recordedfuture parser

--- app/utils/dataFormatters.js
+++ app/utils/dataFormatters.js
@@ -1,14 +1,26 @@
 import Papa from 'papaparse';
 
+const parsers = {
+  csv: (data) => Papa.parse(data, { header: true, skipEmptyLines: true }).data,
+  spaces: (data) => data.toString().split(' ')
+};
+
 export default {
   shodan: (data = []) => {
     return data.map((obj) => {
-      return { ...obj, Data: Papa.parse(obj.Data, { header: true, skipEmptyLines: true }).data };
+      return { ...obj, Data: parsers.csv(obj.Data) };
     });
   },
   recordedfuture: (data = []) => {
     return data.map((obj) => {
-      return { ...obj, Data: Papa.parse(obj.Data, { header: true, skipEmptyLines: true }).data };
+      const spacedPropName = 'Affected Machines: CPE';
+      const result = parsers.csv(obj.Data);
+      result.forEach((item) => {
+        if (item[spacedPropName]) {
+          item[spacedPropName] = parsers.spaces(item[spacedPropName]);
+        }
+      });
+      return { ...obj, Data: result };
     });
   }
 };
