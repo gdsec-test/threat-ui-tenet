e2c470993dd75bcd8b6e44ec8414370182a7ca4a
achatterjee <achatterjee@godaddy.com>
2022-09-27 16:15:05 -0400

    Quicksand file upload

--- app/components/QuickSand.js
+++ app/components/QuickSand.js
@@ -1,82 +1,97 @@
 /* eslint-disable max-len */
-import React, { Component } from 'react';
-import FileUpload from '@ux/file-upload';
 import '@ux/file-upload/dist/styles.css';
+import CloudUpload from '@ux/icon/cloud-upload';
+import '@ux/icon/cloud-upload/index.css';
+import '@ux/icon/x/index.css';
 import '@ux/text-input/dist/styles.css';
+import { Form, Tooltip } from '@ux/uxcore2/lib';
 import { withRouter } from 'next/router';
+import 'rc-tree/assets/index.css';
+import React, { useEffect, useState } from 'react';
+// This page used the react-fine-uploader: https://github.com/FineUploader/react-fine-uploader
+import 'react-fine-uploader/gallery/gallery.css';
 import RenderError from './common/RenderError';
-import { Form, Button, Tooltip } from '@ux/uxcore2/lib';
 
+const acceptedFileTypes =
+  'image/*,video/*,audio/*,.exe,.dll,.pif,.cmd,.bat,.com,.scr,.cpl,.pdf,.doc,.docx,.docm,.xls,.xlsx,.xlsm,.xlsb,.ppt,.pptx,.pptm,.hwp,.jtd,.rft,.xpi,.crx,.eml,.msg,.chm,.js,.vbs,.vbe,,.lnk,.jar,.ps1,.zip,.7z,.rar,.zlib,.asp,.aspx,.apk,.mach-o,.dmg,.app,.xar,.pkg';
 const getTooltip = (caption, message) => (
   <Tooltip openOnHover={true} autoHideTimeout={300} message={message}>
     {caption}
   </Tooltip>
 );
 
-class QuickSand extends Component {
-  constructor() {
-    super(...arguments);
-    this.state = {
-      files: [],
-      acceptedFileTypes:
-        'image/*,video/*,audio/*,.exe,.dll,.pif,.cmd,.bat,.com,.scr,.cpl,.pdf,.doc,.docx,.docm,.xls,.xlsx,.xlsm,.xlsb,.ppt,.pptx,.pptm,.hwp,.jtd,.rft,.xpi,.crx,.eml,.msg,.chm,.js,.vbs,.vbe,,.lnk,.jar,.ps1,.zip,.7z,.rar,.zlib,.asp,.aspx,.apk,.mach-o,.dmg,.app,.xar,.pkg'
-    };
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(fileList) {
-    this.setState({
-      files: fileList
+let createUploader;
+let Gallery;
+if (process.browser) {
+  const FineUploaderTraditional = require('fine-uploader-wrappers').default;
+  Gallery = require('react-fine-uploader').default;
+  createUploader = ({ onComplete }) => {
+    const uploader = new FineUploaderTraditional({
+      options: {
+        chunking: {
+          enabled: true
+        },
+        deleteFile: {
+          enabled: false
+        },
+        request: {
+          endpoint: '/api/quicksand/upload'
+        },
+        retry: {
+          enableAuto: true
+        },
+        callbacks: {
+          onComplete: onComplete
+        }
+      }
     });
-  }
-
-  addTag(e) {
-    if (e.key === 'Enter' || e.keyCode === 13) {
-      e.stopPropagation();
-      e.preventDefault();
-      const newTags = this.state.tags.slice();
-      newTags.push(e.target.value);
-      e.target.value = '';
-      this.setState({ tags: newTags });
-    }
-  }
-
-  render() {
-    return (
-      <Form className={'InputForm'} action=''>
-        <h6>
-          Submit Files
-          {getTooltip('', 'File types supported by Joe Sandbox: ' + this.state.acceptedFileTypes)}
-        </h6>
-
-        <div>
-          <FileUpload
-            name='quicksand-file-upload'
-            accept={this.state.acceptedFileTypes}
-            maxSize={1073741824} // 1 GB
-            multiple={true}
-            onChange={this.onChange}
-            acceptTypeError='We do not accept that file type'
-            maxSizeError='The files exceed our size limit (500 MB size limit per file and 1 GB in total)'
-            duplicateError='You have uploaded this file already!'
-            label='Drag your files here'
-            buttonLabel='Browse'
-          />
-        </div>
-
-        <p></p>
-        {getTooltip('Add Tags', 'Add comma-separated tags to your job (optional)')}
-        <p>
-          <input type='text' label='Add Tags' onKeyDown={this.addTag} name='addtag' />
-        </p>
-        <p></p>
-
-        <Button design='primary' title='Submit' type='submit'>
-          Submit
-        </Button>
-      </Form>
-    );
-  }
+    return uploader;
+  };
 }
 
-export default withRouter(RenderError(QuickSand));
+export const ForensicStorage = () => {
+  const [refresh, setRefresh] = useState(null);
+  const [uploader, setUploader] = useState(null);
+
+  useEffect(() => {
+    if (createUploader) {
+      setUploader(
+        createUploader({
+          onComplete: () => {
+            setRefresh(!refresh);
+          }
+        })
+      );
+    }
+  }, [refresh]);
+
+  return (
+    <Form className={'InputForm'}>
+      <h6>
+        Submit Files
+        {getTooltip('', 'File types supported by Joe Sandbox: ' + acceptedFileTypes)}
+      </h6>
+
+      <div>
+        {process.browser && uploader && (
+          <Gallery
+            uploader={uploader}
+            fileInput-children={`Upload`}
+            dropzone-content={
+              <span className={'react-fine-uploader-gallery-dropzone-content'}>
+                <CloudUpload />
+                {` Upload your files to S3`}
+              </span>
+            }
+          />
+        )}
+      </div>
+
+      <p></p>
+      {getTooltip('Add Tags', 'Add comma-separated tags to your job (optional)')}
+      <input type='text' label='Add Tags' name='addtag' />
+    </Form>
+  );
+};
+
+export default withRouter(RenderError(ForensicStorage));
